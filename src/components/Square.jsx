c6e0de22018ce67477ef0a7cf9eb6244740b8f64
testuser <testuser@gmail.com>
2022-12-21 20:05:22 +0530

    added state and react events

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
+const Square = ({ value, onClick }) => {
 
-const Square = ({ value }) => {
-  return <button type="button" className='square'>{value}</button>;
+  return (
+
+    <button
+      type="button"
+      className="square"
+      onClick={ onClick }> {value}
+    </button>
+  );
 };
 
 export default Square;
